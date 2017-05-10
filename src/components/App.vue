4194c63b7828a0c3c6efbd8cbf708e99fec35814
SunShinewyf <2632807692@qq.com>
2017-05-10 15:22:58 +0800

    add the first project to remote repository

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+     <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import appHeader from './appHeader.vue'
+export default {
+  name: 'app',
+  components: {
+    appHeader
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <header id="header">
+       <nav>
+       <span class="nav-title">{{title}}</span>
+         <ul class="navBtn">
+          <li v-for="item in navItems">
+            <a href="">{{item.name}}</a>
+          </li>
+         </ul>
+       </nav>
+    </header>
+</template>
+
+<script>
+  export default{
+    data () {
+      return {
+        title: 'Multi-objective optimization',
+        navItems: [
+            { name: 'result'},
+            { name: 'optimize'}
+        ]
+      }
+    }
+  }
+</script>
+<style scoped>
+   header{
+     height:50px;
+     background:#e6e6e6;
+   }
+   .nav-title{
+     font-weight: bold;
+     color: inherit;
+     line-height: 1;
+     float: left;
+     padding:0rem 1rem;
+   }
+  .navBtn{
+    display: inline-block;
+   }
+  .navBtn li{
+    display: inline-block;
+   }
+</style>
