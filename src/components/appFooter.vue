01a7a570423ec13a79a918d3686a5e72ef6493e8
SunShinewyf <2632807692@qq.com>
2017-05-22 20:19:27 +0800

    update the footer component

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,5 +1,23 @@
 <template>
-    <footer id="footer"></footer>
+    <footer id="footer">
+       <div class="tips">
+         <p>致力于为出游的小伙伴们，提供物超所值的旅游线路，令你享受旅途每刻</p>
+       </div>
+       <div class="about">
+          <a><p>企业资质</p></a>
+          <a><p>主题旅游</p></a>
+          <a><p>网站地图</p></a>
+       </div>
+       <div class="terms">
+          <a><p>用户协议</p></a>
+          <a><p>隐私条款</p></a>
+          <a><p>帮助中心</p></a>
+       </div>
+       <div class="follow">
+          <a><p>隐私条款</p></a>
+          <a><p>帮助中心</p></a>   
+       </div>
+    </footer>
 </template>
 
 <script>
@@ -16,5 +34,11 @@
         width:100%;
         height:240px;
         background-color:#0c2b48;
+        color:#fff;
+        font-size:14px;
+    }
+    div{
+        float:left;
+        width:25%;
     }
 </style>
