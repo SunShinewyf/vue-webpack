da61bb3307e17a417034d12e94ff62a4c13e65e3
SunShinewyf <2632807692@qq.com>
2017-05-23 20:33:59 +0800

    add the big img to home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
      <app-header></app-header>
+     <div class="header-img">
+     <div class="text">
+        <p class="title">WELCOME TO TRAVEL TIME</p>
+        <div class="line"></div>
+        <p>享受生活和分享喜悦</p>
+     </div>
+     </div>
      <app-footer></app-footer>
   </div>
 </template>
@@ -24,4 +31,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.header-img{
+   width:100%;
+   height:384px;
+   background:url(../assets/header.jpg) no-repeat;
+   margin-top:-30px;
+   background-size:100% 100%;
+}
+.text{
+   color:#fff;
+   text-align:center;
+}
+.text p{
+   font-size:30px;
+}
+.text .title{
+   padding-top:30px;
+}
+.line{
+   border:1px solid #fff;
+   width:356px;
+   margin:0 auto;
+}
 </style>
